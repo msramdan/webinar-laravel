f10f6eab25e8f965e24f803deb37a1cee211e2b4
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2025-04-26 11:40:46 +0700

    add padding

--- resources/views/seminar/sesi.blade.php
+++ resources/views/seminar/sesi.blade.php
@@ -33,7 +33,7 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <div class="table-responsive">
+                        <div class="table-responsive p-1">
                             <table class="table table-hover" id="sesi-table">
                                 <thead>
                                     <tr>
